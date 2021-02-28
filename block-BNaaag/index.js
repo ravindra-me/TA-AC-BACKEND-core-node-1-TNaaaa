ba8e19ef9df051c09614b0c1dffda7ee078daae5
Ravindra Singh <ravindrarajpoot9628172@gmail.com>
2021-02-28 19:19:02 +0530

    node js 2

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,7 +1,10 @@
-var fs = require('fs');
-fs.readFile('./content.md', (err, content) => {
+var { readFile, readFileSync } = require('fs');
+readFile('./content.md', 'utf8', (err, content) => {
   console.log(err, content);
 });
+
+var result = readFileSync('./content.md', 'utf8');
+console.log(result);
 
 let buff1 = Buffer.alloc(10);
 buff1.write('Welcome to buffer');
