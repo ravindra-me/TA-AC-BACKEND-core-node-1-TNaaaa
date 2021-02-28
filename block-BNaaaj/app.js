dfceca804ab1722bf0e2cf15fca9b52716e34027
Ravindra Singh <ravindrarajpoot9628172@gmail.com>
2021-02-28 21:15:19 +0530

    assignment 1

---
+++ block-BNaaaj/app.js
@@ -0,0 +1,5 @@
+let { circleArea, rectangle, squareArea } = require('./area');
+
+console.log(circleArea(2));
+console.log(rectangle(2, 5));
+console.log(squareArea(2));
---
+++ block-BNaaaj/index.js
@@ -0,0 +1,17 @@
+var { cpus, freemem, uptime, version } = require('os');
+console.log(cpus().length, freemem(), uptime(), version());
+var { readFile, unlink } = require('fs');
+
+var buffer1 = Buffer.alloc(15);
+var buffer2 = Buffer.allocUnsafe(15);
+buffer2.write('it is second method to create buffer class');
+buffer1.write('It is First Method to create buffer class');
+console.log(buffer2.toString());
+console.log(buffer1.toString());
+
+const url = require('url');
+const urlParse = url.parse(
+  'https://airindia.com/fares/calculate?from=delhi&to=detroit',
+  true
+);
+console.log(urlParse.query, urlParse.pathname, urlParse.protocol);
